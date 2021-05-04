1946d125085644e09d950b9289e646e6413f49ca
ramona <contact.ramonarusanu@gmail.com>
2021-05-04 14:24:10 +0200

    Store Blog Posts as HTML Files

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/app.css"></link>
+    <title>My blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My first post</a></h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus quaerat doloribus quod aliquid voluptate tenetur odio totam fuga error explicabo perspiciatis, consequuntur dolorum commodi, omnis dicta. Vitae odio voluptates architecto? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus tenetur quidem quisquam unde, explicabo eum. Facere nam debitis, voluptatem obcaecati autem sint voluptate atque ipsa eos veritatis repellendus cum nostrum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed magni officiis adipisci mollitia sapiente temporibus quas nemo libero, autem saepe. A culpa nostrum iusto praesentium mollitia nihil nam non aliquid?</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My second post</a></h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus quaerat doloribus quod aliquid voluptate
+            tenetur odio totam fuga error explicabo perspiciatis, consequuntur dolorum commodi, omnis dicta. Vitae odio
+            voluptates architecto? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus tenetur quidem
+            quisquam unde, explicabo eum. Facere nam debitis, voluptatem obcaecati autem sint voluptate atque ipsa eos
+            veritatis repellendus cum nostrum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed magni officiis
+            adipisci mollitia sapiente temporibus quas nemo libero, autem saepe. A culpa nostrum iusto praesentium mollitia
+            nihil nam non aliquid?</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My third post</a></h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus quaerat doloribus quod aliquid voluptate
+            tenetur odio totam fuga error explicabo perspiciatis, consequuntur dolorum commodi, omnis dicta. Vitae odio
+            voluptates architecto? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus tenetur quidem
+            quisquam unde, explicabo eum. Facere nam debitis, voluptatem obcaecati autem sint voluptate atque ipsa eos
+            veritatis repellendus cum nostrum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed magni officiis
+            adipisci mollitia sapiente temporibus quas nemo libero, autem saepe. A culpa nostrum iusto praesentium mollitia
+            nihil nam non aliquid?</p>
+    </article>
+</body>
+</html>
